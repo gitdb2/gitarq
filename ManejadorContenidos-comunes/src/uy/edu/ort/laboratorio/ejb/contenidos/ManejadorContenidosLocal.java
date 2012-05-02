349177b47caa77cda593020cacb03699968e68fc
Rodrigo Demicheli <rodrigodemi@gmail.com>
2012-05-01 21:06:22 -0300

    saco autor y comentarios

--- ManejadorContenidos-comunes/src/uy/edu/ort/laboratorio/ejb/contenidos/ManejadorContenidosLocal.java
+++ ManejadorContenidos-comunes/src/uy/edu/ort/laboratorio/ejb/contenidos/ManejadorContenidosLocal.java
@@ -1,17 +1,10 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package uy.edu.ort.laboratorio.ejb.contenidos;
 
 import java.util.Date;
 import java.util.List;
 import javax.ejb.Local;
 
-/**
- *
- * @author rodrigo
- */
+
 @Local
 public interface ManejadorContenidosLocal {
     
--- ManejadorContenidos-comunes/src/uy/edu/ort/laboratorio/ejb/contenidos/ManejadorContenidosRemote.java
+++ ManejadorContenidos-comunes/src/uy/edu/ort/laboratorio/ejb/contenidos/ManejadorContenidosRemote.java
@@ -1,15 +1,7 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package uy.edu.ort.laboratorio.ejb.contenidos;
 
 import javax.ejb.Remote;
 
-/**
- *
- * @author rodrigo
- */
 @Remote
 public interface ManejadorContenidosRemote {
     
